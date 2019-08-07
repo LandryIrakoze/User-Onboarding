12fa2fcd95bc88d9910edceb9544e1e7654a15d8
Landry Irakoze <irakozel@gmail.com>
2019-08-07 15:58:22 -0400

    basic form set up

---
+++ user-onboarding/src/components/OnboardingForm.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const OnboardingForm = ({ erros, touched, values, handleSubmit, status }) => {
+
+    const [users, setUsers] = useState([]);
+    console.log(users);
+
+    useEffect(() => {
+        if(status) {
+            setUsers([...users, status])
+        }
+    }, [status])
+
+    return (
+        <Form>
+            <Field type="text" name="name" placeholder="Name"/>
+            <Field type="email" name="email" placeholder="Email"/>
+            <Field type="password" name="password" placeholder="Password"/>
+            <Field type="checkbox" name="tos" placeholder="Terms of Service"/>
+            <button type="submit">submit!</button>
+        </Form>
+    )
+}
+
+const FormikOnboarding = withFormik({
+    mapPropsToValues({ name, email, password, tos }) {
+        return {
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            tos: tos || false
+        }
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required,
+        email: Yup.string().required,
+        password: Yup.string().required
+    }),
+
+    handleSubmit(values, { setStatus }) {
+        axios
+            .post('https://reqres.in/api/users_')
+            .then(res => setStatus(res.data))
+            .catch(err => console.error(err.reponse));
+    }
+
+})(OnboardingForm)
+
+export default FormikOnboarding;
